5ec47e5c1915026047e8ad6c7a3ad2724e338124
Steve Kaliski <stevekaliski@stripe.com>
2025-02-06 12:51:52 -0500

    typescript: add modelcontextprotocol

---
+++ typescript/examples/modelcontextprotocol/src/index.ts
@@ -0,0 +1,30 @@
+import {StripeAgentToolkit} from '@stripe/agent-toolkit/modelcontextprotocol';
+import {StdioServerTransport} from '@modelcontextprotocol/sdk/server/stdio.js';
+
+const server = new StripeAgentToolkit({
+  secretKey: process.env.STRIPE_SECRET_KEY!,
+  configuration: {
+    actions: {
+      paymentLinks: {
+        create: true,
+      },
+      products: {
+        create: true,
+      },
+      prices: {
+        create: true,
+      },
+    },
+  },
+});
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error('Stripe MCP Server running on stdio');
+}
+
+main().catch((error) => {
+  console.error('Fatal error in main():', error);
+  process.exit(1);
+});
---
+++ typescript/src/modelcontextprotocol/toolkit.ts
@@ -0,0 +1,49 @@
+import {McpServer} from '@modelcontextprotocol/sdk/server/mcp.js';
+import {Configuration, isToolAllowed} from '../shared/configuration';
+import StripeAPI from '../shared/api';
+import tools from '../shared/tools';
+
+class StripeAgentToolkit extends McpServer {
+  private _stripe: StripeAPI;
+
+  constructor({
+    secretKey,
+    configuration,
+  }: {
+    secretKey: string;
+    configuration: Configuration;
+  }) {
+    super({
+      name: 'Stripe',
+      version: '0.2.2',
+    });
+
+    this._stripe = new StripeAPI(secretKey, configuration.context);
+
+    const filteredTools = tools.filter((tool) =>
+      isToolAllowed(tool, configuration)
+    );
+
+    filteredTools.forEach((tool) => {
+      this.tool(
+        tool.method,
+        tool.description,
+        tool.parameters.shape,
+        // @ts-expect-error
+        async (arg: any) => {
+          const result = await this._stripe.run(tool.method, arg);
+          return {
+            content: [
+              {
+                type: 'text',
+                text: result,
+              },
+            ],
+          };
+        }
+      );
+    });
+  }
+}
+
+export default StripeAgentToolkit;
