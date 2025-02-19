17ba666dba2a3f38af8b569e4fcfacde24a4e412
Steve Kaliski <stevekaliski@stripe.com>
2025-02-19 13:40:25 -0500

    copy over lint/config/etc from typescript dir

--- modelcontextprotocol/jest.config.ts
+++ modelcontextprotocol/jest.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "jest";
+import type {Config} from 'jest';
 
 const config: Config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/test/**/*.ts?(x)"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/test/**/*.ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
 
 export default config;
